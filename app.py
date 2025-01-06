8cde11aea58dbde2d5ebca4d35a0972981cbdf76
Thomas C Bevan <94372892+Thomas-Bevan@users.noreply.github.com>
2025-01-06 21:34:47 +0000

    updated requirements.txt and removed testing from app

--- app.py
+++ app.py
@@ -10,13 +10,6 @@ app.add_api(config.basedir / "swagger.yml")
 def home():
     return render_template("home.html")
 
-with app.app.app_context():
-    try:
-        trails = Trail.query.all()
-        print(f"Retrieved {len(trails)} trails from the database.")
-        print(trails)
-    except Exception as e:
-        print(f"Error while querying trails: {e}")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
